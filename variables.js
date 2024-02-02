6ea79f402cad6afea8aff7776c97662456f4d69f
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-02-02 12:50:59 +0530

    Third commit on 2nd Feb

--- variables.js
+++ variables.js
@@ -43,6 +43,8 @@ let z;
  z=5;
  console.log(z);
 
+
+// Point No 01
 /*Always use const if the value should not be changed
 
   Always use const if the type should not be changed (Arrays and Objects)
@@ -50,3 +52,26 @@ let z;
   Only use let if you can't use const
 
   Only use var if you MUST support old browsers.*/
+
+
+// Point No 02
+//One Statement, Many Variables
+ let person = "John Doe", carName = "Volvo", price = 200;
+
+
+//Point No 03
+//A variable declared without a value will have the value undefined.
+let country;
+
+//Point No 04
+//Re-Declaring JavaScript Variables
+var CarName = "Volvo";
+var CarName;
+//The variable carName will still have the value "Volvo" after the execution of these statements:
+
+/*let toyName = "Volvo";
+let toyName;
+You cannot re-declare a variable declared with let or const.*/
+
+
+
