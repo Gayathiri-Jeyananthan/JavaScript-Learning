5a24f8a8987d9d38b4032e84fd91088437d5ba4f
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-02-02 12:38:24 +0530

    First commit on 2nd Feb

--- Day 1.js
+++ Day 1.js
@@ -1,3 +1,25 @@
 function myFunc() {
     document.getElementById("third").innerHTML = "I have successfully changed"
 }
+
+// Point No 1
+/*A JavaScript name must begin with:
+A letter (A-Z or a-z)
+A dollar sign ($)
+Or an underscore (_)*/
+
+
+// Point No 2
+//JavaScript is Case Sensitive
+
+
+// Point No 3
+//Numbers are not allowed as the first character in names.
+
+// Point No 4
+//Hyphens are not allowed in JavaScript. They are reserved for subtractions.instead od that u can use underscore
+
+
+
+
+
