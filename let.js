b25a770705f5d7a23b4c291a3d366c3010c8375c
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-02-05 11:12:20 +0530

    first commit on 5th Feb

---
+++ let.js
@@ -0,0 +1,12 @@
+//let keyword was introduced in ES6 (2015).
+//Variables declared with let have block scope.
+//Variables declared with let must be Declared before use
+//Variables declared with let cannot be Redeclared in the same scope
+
+{
+  let x = 2;
+  }
+
+  console.log(x);
+
+  
