d6425ca4288605bbf1a79f5f2eb88cc7c6e5cade
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-02-06 11:01:16 +0530

    Second update on 6th Feb

--- let.js
+++ let.js
@@ -4,19 +4,49 @@
 //Variables declared with let cannot be Redeclared in the same scope
 
 //Point No 01
-//(Block Scope)
+//(let and const Block Scope)
 //let Variables declared inside a { } block cannot be accessed from outside the block:
 {
   let x = 2;
   }
 // X can not be used here
 
-//Point No 02
-//(Global Scope)
+
+//(var Global Scope)
 {
     var x = 2;
   }
-  // x can be used here
+// x can be used here
+
+//Point No 02
+//Can not be redeclared
+let x =3;
+console.log(x); //It cause syntax error. because we can't redeclare 
+//(SyntaxError: Identifier 'x' has already been declared )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
