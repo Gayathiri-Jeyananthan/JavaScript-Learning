aa1d5e81602ce3161241b73dbe1655e81a5789d5
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-02-07 14:01:40 +0530

    Third update on 7th Feb

--- const.js
+++ const.js
@@ -2,6 +2,20 @@
 //Variables defined with const cannot be Reassigned
 //Variables defined with const have Block Scope
 
+//Point No 01
+//Cannot be Reassigned
+const PI = 3.141592653589793;
+PI = 3.14;      // This will give an error
+
+//Point No 02
+//Must be Assigned
+
+const x = 3; //This is correct
+
+const y; ////Must be Assigned
+y=5;//This will give an error
+
+
 
 
 
